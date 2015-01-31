b4782ef1d158cddcbcca03af8c95994c9ad54a78
Moinuddin Sheriff <moinuddin@prazas.com>
2015-01-31 21:29:38 +0530

    Saving Data API

--- caluclate_saving.php
+++ caluclate_saving.php
@@ -1,7 +1,42 @@
-$source 
-$destination
+<?php 
+//$source 
+//$destination
 
 //Calcualte distance, time 
 //Calcuate fare
 //return 
 //hotel price
+
+include("geo_calculator.php");
+
+$source = $_GET['usersource'];
+$destination = $_GET['destination'];
+getSaving($source, $destination);
+function getSaving($source, $destination){
+    $data = getDistance($source, $destination);
+    $fare = calculateFare($data["distance"]);
+    $duration = minToHours(calculateDuration($data["distance"]));
+    
+    $start_date = "06/03/2015";
+    $end_date = "09/03/2015";
+    $hotelPrice = getStayzillahotel($destination, $start_date, $end_date);
+    $Price = $hotelPrice->hotels[0]->price;
+    $stayzillaSource = $hotelPrice->hotels[0]->address;
+    
+    $data_User = array("distance" => $data["distance"], "duration" => $duration, 
+        "cabFare" => $fare);
+    
+    $stayzillaData = getDistance($stayzillaSource, $destination);
+    $stayzillafare = calculateFare($stayzillaData["distance"]);
+    $stayzillaDuration = minToHours(calculateDuration($stayzillaData["distance"]));
+    
+    $data_stayzilla = array("distance" => $stayzillaData["distance"], "duration" => $stayzillaDuration, 
+        "cabFare" => $stayzillafare, "hotelFare" => $Price, "hotelData" => $hotelPrice->hotels[0]);
+    
+    $responceData = array();
+    $responceData["user"] = $data_User;
+    $responceData["stayzilla"] = $data_stayzilla;
+    
+    return json_encode($responceData);
+}
+?>
--- geo_calculator.php
+++ geo_calculator.php
@@ -5,9 +5,62 @@ function getDistance($source, $destination) {
     curl_setopt($ch, CURLOPT_URL, $apiUrl);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $distanceResult = json_decode(curl_exec($ch));
+    
     return array("distance" => $distanceResult->rows[0]->elements[0]->distance->text, "duration" => $distanceResult->rows[0]->elements[0]->duration->text);
 }
 
 function calculateFare($distance) {
     return $distance * 15 + 50;
 }
+
+function calculateDuration($distance) {
+    return $distance * 4 + 10;
+}
+
+function minToHours($mins) { 
+    $mm = $mins;
+    $hr = floor($mm/60);
+    if($hr != '00'){
+        return sprintf("%02dh %02dmin", floor($mm/60), $mm%60);
+    }else{
+        return sprintf("%02dmin", $mm%60);
+    }
+    } 
+
+function getStayzillahotel($destination_address, $start_date, $end_date) {
+        $url = 'http://180.92.168.7/hotels';
+        $fields = array(
+            'location' => urlencode($destination_address),
+            'checkin' => urlencode($start_date),
+            'checkout' => urlencode($end_date),
+            'property_type' => "Hotels",
+        );
+
+//url-ify the data for the POST
+        foreach ($fields as $key => $value) {
+            $fields_string .= $key . '=' . $value . '&';
+        }
+        rtrim($fields_string, '&');
+
+//open connection
+        $ch = curl_init();
+        $agent= 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)';
+
+//set the url, number of POST vars, POST data
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded')); 
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);    
+        curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_USERAGENT, $agent);
+    
+// receive server response ...
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+//execute post
+        $result = curl_exec($ch);
+        $result = json_decode($result);
+        return $result;
+//close connection
+        curl_close($ch);
+    }
