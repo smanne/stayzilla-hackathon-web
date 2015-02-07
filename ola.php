87bbcfe53209f3976b13387d3f746f14bd0b71b3
Kiran <kiran@prazas.com>
2015-02-07 23:20:38 +0530

    added get user
    added get user

--- ola.php
+++ ola.php
@@ -6,22 +6,14 @@
  * and open the template in the editor.
  */
 
-//This example is a sample video upload stored in parse
-echo $r;
-    $parse = new parseObject('Videos');
-    $parse->title = $data['upload_data']['title'];
-    $parse->description = $data['upload_data']['description'];
-    $parse->tags = $data['upload_data']['tags'];
+echo queryUsersWithQueryExpectResultsKey();
 
-    //create new geo
-    $geo = new parseGeoPoint($data['upload_data']['lat'],$data['upload_data']['lng']);
-    $parse->location = $geo->location;
 
-    //use pointer to other class
-    $parse->userid = array("__type" => "Pointer", "className" => "_User", "objectId" => $data['upload_data']['userid']);
 
-    //create acl
-    $parse->ACL = array("*" => array("write" => true, "read" => true));
-    $r = $parse->save();
-    
-    echo $r;
+ function queryUsersWithQueryExpectResultsKey(){
+		$userQuery = new parseQuery('users');
+		$userQuery->whereExists('type');
+		$return = $userQuery->find();
+                
+                return $return;
+	}
